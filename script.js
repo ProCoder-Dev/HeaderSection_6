fde17afcd4203f9e8d783b9dad95c327b8dbca1e
ProCoder-Dev <nazmulhuda.dev1@gmail.com>
2025-05-13 13:01:57 +0600

    header-sec

---
+++ script.js
@@ -0,0 +1,4 @@
+let nav = document.querySelector('.nav');
+let mobile_menu_icon = document.querySelector('.mobile_menu_icon');
+
+mobile_menu_icon.addEventListener('click', () => nav.classList.toggle('active'))
